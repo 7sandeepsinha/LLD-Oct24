806b2211bc8e1dfff6d533e2e50731f409daa856
sndpoffcl <sandeep2official@gmail.com>
2025-01-22 22:46:26 +0530

    TicTacToe started - models done

---
+++ src/projects/TicTacToe/model/Bot.java
@@ -0,0 +1,15 @@
+package projects.TicTacToe.model;
+
+import projects.TicTacToe.model.constants.BotDifficultyLevel;
+
+public class Bot extends Player{
+    private BotDifficultyLevel botDifficultyLevel;
+
+    public BotDifficultyLevel getBotDifficultyLevel() {
+        return botDifficultyLevel;
+    }
+
+    public void setBotDifficultyLevel(BotDifficultyLevel botDifficultyLevel) {
+        this.botDifficultyLevel = botDifficultyLevel;
+    }
+}
---
+++ src/projects/TicTacToe/model/constants/GameState.java
@@ -0,0 +1,5 @@
+package projects.TicTacToe.model.constants;
+
+public enum GameState {
+    IN_PROGRESS, YET_TO_START, WINNER, DRAW;
+}
